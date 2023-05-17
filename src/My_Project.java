0efb671989a1d0fa82aec5438f48952455b1a638
SANJAYLOHANA <sklohana232@gmail.com>
2023-05-17 11:42:23 +0500

    Modified File

--- src/My_Project.java
+++ src/My_Project.java
@@ -1,4 +1,3 @@
-
 class myProject1 {
 int sum;
 int my_Calculator(int num1, int num2){
@@ -14,11 +13,18 @@ class myproject2 extends myProject1{
     return sub;
    }
 }
-
+class myproject3 extends myProject2{
+   int multi;
+   int my_Calculator3(int num1, int num2){
+    multi = num1 * num2;
+    return multi;
+   }
+}
 public class My_Project { 
 public static void main(String[] args) {
      myproject2 my = new myproject2();
      System.out.println("The Sum Of Given Integers Is: "+my.my_Calculator(10, 20));
      System.out.println("The Sub Of Given Integers Is: "+my.my_Calculator2(20, 10));
+     System.out.println("The Sub Of Given Integers Is: "+my.my_Calculator3(20, 10));
     }
 }
